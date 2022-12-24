b072a00b8f4b725341d4b02822b46dd89ba4df7e
Garry Xiao <xz@etsoo.com>
2022-12-24 15:35:47 +1300

    Upgrade dependencies

--- src/site/ClientSite.ts
+++ src/site/ClientSite.ts
@@ -138,65 +138,73 @@ export class ClientSite {
         share?: wx.UpdateAppMessageShareDataParams,
         api = 'Public/CreateJsApiSignature'
     ) {
-        // Check exists
-        if (typeof wx === undefined) return;
+        try {
+            // Check exists
+            if (typeof wx === undefined) return;
 
-        // Load config
-        const data = await this.api.put<wx.ConfigBase>(
-            api,
-            {
-                url: location.href
-            },
-            { showLoading: false }
-        );
+            // Load config
+            const data = await this.api.put<wx.ConfigBase>(
+                api,
+                {
+                    url: location.href
+                },
+                { showLoading: false }
+            );
 
-        if (data == null) return;
+            if (data == null) return;
 
-        // Apis
-        const apis: wx.ApiName[] = [
-            'updateAppMessageShareData',
-            'onMenuShareAppMessage',
-            'updateTimelineShareData',
-            'onMenuShareTimeline'
-        ];
+            // Apis
+            const apis: wx.ApiName[] = [
+                'updateAppMessageShareData',
+                'onMenuShareAppMessage',
+                'updateTimelineShareData',
+                'onMenuShareTimeline'
+            ];
 
-        // Config
-        const result = await wxe.configAsync({
-            ...data,
-            jsApiList: apis
-        });
+            // Config
+            const result = await wxe.configAsync({
+                ...data,
+                jsApiList: apis
+            });
 
-        if (result != null) {
-            console.log('wxe.configAsync', result);
-            return;
+            if (result != null) {
+                console.log('wxe.configAsync', result);
+                return;
+            }
+
+            // Check
+            const ckResult = await wxe.checkJsApiAsync({ jsApiList: apis });
+
+            if (!ckResult.errMsg.endsWith('ok')) {
+                console.log('checkJsApiAsync', ckResult.errMsg);
+            }
+
+            // Share data
+            if (share == null) {
+                const title = this.getOG('title') ?? document.title;
+                const link = this.getOG('url') ?? location.href;
+                const desc =
+                    this.getOG('description') ??
+                    this.getMeta('description') ??
+                    '';
+
+                const imgUrl =
+                    this.getOG('image') ??
+                    this.getMeta('image_src') ??
+                    '/og.jpg';
+
+                share = {
+                    title,
+                    link,
+                    imgUrl,
+                    desc
+                };
+            }
+
+            // Setup share
+            wxe.setupShare(share, ckResult.checkResult);
+        } catch (e) {
+            console.log('WX setup failed with an error', e);
         }
-
-        // Check
-        const ckResult = await wxe.checkJsApiAsync({ jsApiList: apis });
-
-        if (!ckResult.errMsg.endsWith('ok')) {
-            console.log('checkJsApiAsync', ckResult.errMsg);
-        }
-
-        // Share data
-        if (share == null) {
-            const title = this.getOG('title') ?? document.title;
-            const link = this.getOG('url') ?? location.href;
-            const desc =
-                this.getOG('description') ?? this.getMeta('description') ?? '';
-
-            const imgUrl =
-                this.getOG('image') ?? this.getMeta('image_src') ?? '/og.jpg';
-
-            share = {
-                title,
-                link,
-                imgUrl,
-                desc
-            };
-        }
-
-        // Setup share
-        wxe.setupShare(share, ckResult.checkResult);
     }
 }
